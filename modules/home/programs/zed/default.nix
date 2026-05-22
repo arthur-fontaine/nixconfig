8cf02ca974b502647203c1612718fcc7b0c2ed5d
Arthur Fontaine <0arthur.fontaine@gmail.com>
2026-05-22 20:02:38 +0200

    fix(darwin): fix Zed keymap writability

--- modules/home/programs/zed/default.nix
+++ modules/home/programs/zed/default.nix
@@ -1,11 +1,11 @@
 { config, lib, ... }:
 {
-  xdg.configFile."zed/keymap.json".source = ./keymap.json;
-
-  # Copy settings.json instead of symlinking so Zed can write to it
+  # Copy settings.json and keymap.json instead of symlinking so Zed can write to them
   # (e.g. when installing extensions). Resets to managed version on each activation.
   home.activation.copyZedSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD cp -f ${./settings.json} ${config.xdg.configHome}/zed/settings.json
     $DRY_RUN_CMD chmod u+w ${config.xdg.configHome}/zed/settings.json
+    $DRY_RUN_CMD cp -f ${./keymap.json} ${config.xdg.configHome}/zed/keymap.json
+    $DRY_RUN_CMD chmod u+w ${config.xdg.configHome}/zed/keymap.json
   '';
 }
